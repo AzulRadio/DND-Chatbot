78333859e7554ec018277adc7f6d38d1e5c98226
AzulRadio <50132891+AzulRadio@users.noreply.github.com>
2019-10-22 23:58:17 -0500

    Add files via upload
    +attack & damage
    +check(adv,disadv, natural 20 and natural 1)

--- MonsterSpawn.py
+++ MonsterSpawn.py
@@ -1,14 +1,17 @@
 import random
 
 dragon_dict = {'name':'dragon','ID':30334,
-               'Str':(3,6,0), 'Dex':(3,6,0), 'Con':(3,6,0), 'Int':(3,6,0), 'Wis':(3,6,0), 'Cha':(3,6,0), 'Health':(5,8,0)}      # A dictionary that includes all attributes of dragon
+               'Str':(3,6,0), 'Dex':(3,6,0), 'Con':(3,6,0), 'Int':(3,6,0), 'Wis':(3,6,0), 'Cha':(3,6,0), 'Health':(5,8,0),'attack1':(2,1,4,2)}      # A dictionary that includes all attributes of dragon
                                                                                                                                 # The tuples corresponding to each key refer to the roll_dice parameters (num, sides, adjust)
 goblin_dict = {'name':'goblin','ID':34589,
                'Str':(3,6,0), 'Dex':(3,6,0), 'Con':(3,6,0), 'Int':(3,6,0), 'Wis':(3,6,0), 'Cha':(3,6,0), 'Health':(0,0,15)}
 
+human_dict = {'name':'dragon','ID':10001,
+               'Str':(3,6,0), 'Dex':(3,6,0), 'Con':(3,6,0), 'Int':(3,6,0), 'Wis':(3,6,0), 'Cha':(3,6,0), 'Health':(5,8,0), 'attack1':(2,1,4,2)}
 
 Monster_dict = {30334: dragon_dict,
-                34589: goblin_dict}    # The one-full-dictionary containing all attributes of all monsters
+                34589: goblin_dict,
+                10001: human_dict}    # The one-full-dictionary containing all attributes of all monsters
                                       # TO DO: make it a json file
 
 '''
@@ -28,16 +31,17 @@ def convert_id_to_attri(ID):
 
 
 class Monster:
-    def __init__(self, attri_dict, name, ID=0, Str=0, Dex=0 , Con=0, Int=0, Wis=0, Cha=0, Health=0):
+    def __init__(self, abiliscore_dict, name, ID=0, Str=0, Dex=0 , Con=0, Int=0, Wis=0, Cha=0, Health=0):
         self.name = name
-        self.ID = attri_dict ['ID']
-        self.Str = calc_adjust_value(roll_dice(attri_dict['Str']))
-        self.Dex = calc_adjust_value(roll_dice(attri_dict['Dex']))
-        self.Con = calc_adjust_value(roll_dice(attri_dict['Con']))
-        self.Int = calc_adjust_value(roll_dice(attri_dict['Int']))
-        self.Wis = calc_adjust_value(roll_dice(attri_dict['Wis']))
-        self.Cha = calc_adjust_value(roll_dice(attri_dict['Cha']))
-        self.Health = roll_dice(attri_dict['Health'])
+        self.ID = abiliscore_dict ['ID']
+        self.Str = calc_score_modifier( roll_dice( abiliscore_dict['Str'] ) )
+        self.Dex = calc_score_modifier( roll_dice( abiliscore_dict['Dex'] ) )
+        self.Con = calc_score_modifier( roll_dice( abiliscore_dict['Con'] ) )
+        self.Int = calc_score_modifier( roll_dice( abiliscore_dict['Int'] ) )
+        self.Wis = calc_score_modifier( roll_dice( abiliscore_dict['Wis'] ) )
+        self.Cha = calc_score_modifier( roll_dice( abiliscore_dict['Cha'] ) )
+        self.Health = roll_dice(abiliscore_dict['Health'])
+        self.live = 1
     def display_to_dm(self):
         print ("ID = ",self.ID ,"\nname = ",self.name,
                "\nStr = ",self.Str,"\nDex = ",self.Dex,
@@ -45,14 +49,88 @@ class Monster:
                "\nWis = ",self.Wis,"\nCha = ",self.Cha,
                "\nHealth = ",self.Health,"\n")
     def display_to_player(self):
-        print("name = ",self.name)
+        print("name = %s\n"%(self.name))
     def heal_or_damage(self, heal_or_damage):
         heal_or_damage = heal_or_damage//1
         self.Health = self.Health + heal_or_damage
+        if(self.Health <= 0):
+            self.live = 0
+        else:
+            self.live = 1
+        self.show_health()
     def show_health(self):
-        print("name = ",self.name,"\nHealth = ",self.Health,"\n")
+        print("%s, Health = %d\n" %(self.name, self.Health))
 
+    def check_adv_dis(self, state):
+        if (state == "adv"):
+            check1 = random.randint(1,20)
+            check2 = random.randint(1,20)
+            check = max(check1, check2)
+            print("Adv in checking : %d, %d. result : %d"%(check1,check2,check))
+        elif (state == "dis"):
+            check1 = random.randint(1,20)
+            check2 = random.randint(1,20)
+            check = min(check1, check2)
+            print("Disadv in checking : %d, %d. result : %d"%(check1,check2,check))
+        else:
+            check = random.randint(1,20)
+        return check
 
+    def check(self, abili, state = 0):
+        check = self.check_adv_dis(state)
+        if(check == 20):
+            print("Check {0} {1} = Natural 20\n".format(self.name, abili))
+        elif(check == 1):
+            print("Check {0} {1} = Natural l\n".format(self.name, abili))
+        else:
+            if(type(abili) == type(1)):
+                if(abili >=0):
+                    print("Check {0} {1} = {2} + {3} = {4}\n".format(self.name, abili, check, abili, check+abili))
+                else:
+                    print("Check {0} {1} = {2} - {3} = {4}\n".format(self.name, abili, check, abili, check+abili))
+                return
+            if(getattr(self, abili) >= 0):
+                print("Check {0} {1} = {2} + {3} = {4}\n".format(self.name, abili, check, getattr(self, abili), check+getattr(self, abili)))
+            else:
+                print("Check {0} {1} = {2} - {3} = {4}\n".format(self.name, abili, check, getattr(self, abili)*(-1), check+getattr(self, abili)))
+                return
+                            #all these if-else make print look nice
+            
+    def attack(self, state = 0):
+        if(self.live == 0):
+            return
+        abiliscore_dict = Monster_dict[self.ID]
+        i = 1
+        while('attack'+str(i) in abiliscore_dict):
+            damage = 0
+            hit_bonus, dice_num, dice_surfaces, fixed_damage = abiliscore_dict['attack'+str(i)]
+            i+=1
+            attack_check = self.check_adv_dis(state)
+            if(attack_check == 20):
+                print("Attack check by {0} on {1} = Natural 20\n".format(self.name, ""))
+                damage = dice_num * dice_surfaces + fixed_damage
+                print("damage = %d\n"%(damage))
+            elif(attack_check == 1):
+                print("Attack check by {0} on {1} = Natural l\n".format(self.name, ""))
+                damage = 0
+                print("damage = 0")
+            else:
+                print("Attack check by {0} on {1} = {2}".format(self.name, "", attack_check))
+                damage = roll_dice((dice_num, dice_surfaces, fixed_damage))
+                print("damage = %d\n"%(damage))
+                        #Now the damage produced by this function can't be taken by another object
+                #Todo: use heal_or_damage() to put damage on another object
+                
+    def designate_target(self, target_total , target_designated = 0):
+        if(target_designated > 0):
+            print("Designate target %d\n"%(target_designated))
+            target = target_designated
+        else:
+            target = random.randint(1,target_total)
+            print("Designate target %d\n"%(target))
+        return target
+            
+            
 #Func name: roll_dice()
 #Parameter: tuple with format (num, sides, adjustment)
 #Precondition:num, sides and adjustment are positive integer
@@ -65,8 +143,10 @@ def roll_dice(ndxpy):
         n -= 1
     return sum + y
 
-def calc_adjust_value(attri_value):
-    return (attri_value-10)//2
+    
+
+def calc_score_modifier(abili_score):
+    return (abili_score-10)//2
 
 
 #Func name:Spawn_Multi_Monsters
@@ -74,20 +154,23 @@ def calc_adjust_value(attri_value):
 #Precondition: get attri_dict from Monster_dict
 #Postcondition: designated number of members of Class Monster will be spawn and Display To DM will be called
 def Spawn_Multi_Monsters(ID, num):
-    attri_dict = Monster_dict[ID]
+    abiliscore_dict = Monster_dict[ID]
     for i in range(num):
-        name = attri_dict['name']+str(i+1)
-        globals()[name] = Monster(attri_dict, name)
-        globals()[name].display_to_dm()
+        name = abiliscore_dict['name']+str(i+1)
+        globals()[name] = Monster(abiliscore_dict, name)
+##        globals()[name].display_to_dm()
 
 #************* test code below ****************#
 
 Spawn_Multi_Monsters(30334,3)
-Spawn_Multi_Monsters(34589,2)
+##Spawn_Multi_Monsters(34589,2)
 dragon1.show_health()
 dragon1.heal_or_damage(-3)
-dragon1.show_health()
-dragon2.show_health()
-#spawn 3 monsters with ID 30334
-#check convert_id_to_attri() for more details
-#whatever ID you put you can only spawn dragon
+dragon2.display_to_player()
+dragon1.check('Con', 'dis')
+dragon1.check('Str', 'adv')
+dragon1.live = 0
+print(dragon1.live)
+dragon2.attack()
+
+
